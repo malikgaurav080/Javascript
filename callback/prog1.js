52ee2aa903aa70f9b004ae6e73c3b872540af803
malikgaurav080 <malikgaurav080@gmail.com>
2023-09-17 20:55:40 +0530

    Callback, SyncVs Async and setTimeout

---
+++ callback/prog1.js
@@ -0,0 +1,14 @@
+//If we are passing function as a parameter in another function then the function is called callback function.
+// function
+function greet(name, callback) {
+    console.log('Hi' + ' ' + name);
+    callback();
+}
+
+// callback function
+function callMe() {
+    console.log('I am callback function');
+}
+
+// passing function as an argument
+greet('Peter', callMe);
